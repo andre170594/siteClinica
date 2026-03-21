5043d264a0331d956759de774f6017d11adfbec1
andre <andre_pessoa99@hotmail.com>
2026-03-21 17:22:24 +0000

    v0.7

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,14 +180,31 @@ overlay.addEventListener("click", closeCard);
 
 
 // equipa
-const teamCards = document.querySelectorAll(".team-card")
+const cardsTeam = document.querySelectorAll('.team-card');
 
-teamCards.forEach(card => {
+let index = 0;
+let interval = null;
 
-    card.addEventListener("click", () => {
+function startAutoRotation() {
+    interval = setInterval(() => {
+        cardsTeam.forEach(c => c.classList.remove('auto-active'));
 
-        card.classList.toggle("open")
+        cardsTeam[index].classList.add('auto-active');
 
-    })
+        index = (index + 1) % cardsTeam.length;
+    }, 3000); // 3 segundos (ajusta aqui)
+}
 
-})
+function stopAutoRotation() {
+    clearInterval(interval);
+    cardsTeam.forEach(c => c.classList.remove('auto-active'));
+}
+
+/* iniciar */
+startAutoRotation();
+
+/* parar quando hover */
+document.querySelector('.team-grid').addEventListener('mouseenter', stopAutoRotation);
+
+/* voltar quando sai */
+document.querySelector('.team-grid').addEventListener('mouseleave', startAutoRotation);
